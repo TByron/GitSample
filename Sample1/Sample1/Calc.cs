533449289e77f0e28015facba731989535929975
adam.szabo <adam.szabo@SWS00592>
2017-01-11 09:33:31 +0100

    Div

--- Sample1/Sample1/Calc.cs
+++ Sample1/Sample1/Calc.cs
@@ -8,9 +8,27 @@ namespace Sample1
 {
     public class Calc
     {
+        /// <summary>
+        /// Add
+        /// </summary>
+        /// <param name="a">input a</param>
+        /// <param name="b">input b</param>
+        /// <returns>a+b</returns>
         public int Add(int a, int b)
         {
             return a + b;
         }
+
+        /// <summary>
+        /// divied
+        /// </summary>
+        /// <param name="a">nominator a</param>
+        /// <param name="b">denominator b</param>
+        /// <returns>a/b</returns>
+        public double Div(double a, double b)
+        {
+            return a/b;
+        }
+
     }
 }
