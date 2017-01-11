d01e056016ad296657f16c5a27033a6f0f9d0b01
adam.szabo <adam.szabo@SWS00592>
2017-01-11 09:44:34 +0100

    ICalc created

--- Sample1/Sample1/Calc.cs
+++ Sample1/Sample1/Calc.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Sample1
 {
-    public class Calc
+    public class Calc: ICalc
     {
         /// <summary>
         /// Add
---
+++ Sample1/Sample1/ICalc.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample1
+{
+    public interface ICalc
+    {
+        int Add(int a, int b);
+        double Div(double a, double b);
+        double DivD(double a, double b, double c);
+    }
+}
